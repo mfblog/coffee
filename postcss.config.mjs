cfb6ae4f9444e9716418acf7cccc76907db22ca9
chu3 <65387067+chu3@users.noreply.github.com>
2025-04-19 08:18:56 +0800

    优化：兼容性

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -7,10 +7,32 @@ const config = {
         'nesting-rules': false,
         'custom-properties': false
       },
-      stage: 3
+      stage: 3,
+      browsers: [
+        'last 2 versions',
+        '> 1%',
+        'iOS >= 9',
+        'Android >= 4.4',
+        'not dead'
+      ],
+      autoprefixer: {
+        flexbox: true,
+        grid: true
+      }
     },
-    'postcss-normalize': {},
-    autoprefixer: {},
+    'postcss-normalize': {
+      browsers: [
+        'last 2 versions',
+        '> 1%',
+        'iOS >= 9',
+        'Android >= 4.4',
+        'not dead'
+      ]
+    },
+    autoprefixer: {
+      flexbox: true,
+      grid: true
+    },
   },
 };
 
