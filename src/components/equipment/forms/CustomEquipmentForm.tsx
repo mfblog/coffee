3588a9b5cdfabee8531932871720dad4d6b280cf
chu3 <65387067+chu3@users.noreply.github.com>
2025-05-17 10:05:43 +0800

    更新：在 CustomEquipmentForm 组件中移除意式机的说明信息，优化代码结构，提升可读性。

--- src/components/equipment/forms/CustomEquipmentForm.tsx
+++ src/components/equipment/forms/CustomEquipmentForm.tsx
@@ -1278,25 +1278,7 @@ const CustomEquipmentForm: React.FC<CustomEquipmentFormProps> = ({
 
                             </div>
                         </div>
-
-                        {/* 器具说明 */}
-                        {selectedPreset === 'espresso' && (
-                            <div className="px-4 pb-4">
-                                <div className="rounded-lg bg-blue-50 dark:bg-blue-900/20 p-3 text-sm text-blue-600 dark:text-blue-400">
-                                    <div className="flex items-start">
-                                        <div className="mr-2 mt-0.5 flex-shrink-0">
-                                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M13 16H12V12H11M12 8H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                            </svg>
-                                        </div>
-                                        <div>
-                                            意式机不需要杯型设置和注水动画。您可以直接设置基本信息后保存。
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-
+                        
                         {/* 杯型设置 */}
                         {selectedPreset !== 'espresso' && (
                         <div className="p-4">
