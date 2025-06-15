450c9f957329187d12a3be9746fe68878d75d60b
chu3 <1507439584@qq.com>
2025-06-15 20:57:41 +0800

    还原：夏季八冲

--- src/lib/core/config.ts
+++ src/lib/core/config.ts
@@ -660,6 +660,28 @@ export const brewingMethods: BrewingMethods = {
 				],
 			},
 		},
+		{
+			name: "夏季八冲(全宇宙最多人使用的咖啡冲煮手法)",
+			params: {
+				coffee: "(略)",
+				water: "(略)",
+				ratio: "(略)",
+				grindSize: "(略)",
+				temp: "(略)",
+				videoUrl: "",
+				roastLevel: "(略)",
+				stages: [
+					{
+						time: 0,
+						pourTime: 0,
+						label: "(略)",
+						water: "0g",
+						detail: "(略)",
+						pourType: "other",
+					},
+				],
+			},
+		},
 	],
 	CleverDripper: [
 		{
@@ -762,6 +784,28 @@ export const brewingMethods: BrewingMethods = {
 				],
 			},
 		},
+		{
+			name: "夏季八冲(全宇宙最多人使用的咖啡冲煮手法)",
+			params: {
+				coffee: "(略)",
+				water: "(略)",
+				ratio: "(略)",
+				grindSize: "(略)",
+				temp: "(略)",
+				videoUrl: "",
+				roastLevel: "(略)",
+				stages: [
+					{
+						time: 0,
+						pourTime: 0,
+						label: "(略)",
+						water: "0g",
+						detail: "(略)",
+						pourType: "other",
+					},
+				],
+			},
+		},
 	],
 	Kalita: [
 		{
@@ -802,6 +846,28 @@ export const brewingMethods: BrewingMethods = {
 				],
 			},
 		},
+		{
+			name: "夏季八冲(全宇宙最多人使用的咖啡冲煮手法)",
+			params: {
+				coffee: "(略)",
+				water: "(略)",
+				ratio: "(略)",
+				grindSize: "(略)",
+				temp: "(略)",
+				videoUrl: "",
+				roastLevel: "(略)",
+				stages: [
+					{
+						time: 0,
+						pourTime: 0,
+						label: "(略)",
+						water: "0g",
+						detail: "(略)",
+						pourType: "other",
+					},
+				],
+			},
+		},
 	],
 	Origami: [
 		{
@@ -838,6 +904,28 @@ export const brewingMethods: BrewingMethods = {
 						water: "225g",
 						detail: "中心定点注水，降低萃取率",
 						pourType: "center",
+					},
+				],
+			},
+		},
+		{
+			name: "夏季八冲(全宇宙最多人使用的咖啡冲煮手法)",
+			params: {
+				coffee: "(略)",
+				water: "(略)",
+				ratio: "(略)",
+				grindSize: "(略)",
+				temp: "(略)",
+				videoUrl: "",
+				roastLevel: "(略)",
+				stages: [
+					{
+						time: 0,
+						pourTime: 0,
+						label: "(略)",
+						water: "0g",
+						detail: "(略)",
+						pourType: "other",
 					},
 				],
 			},
